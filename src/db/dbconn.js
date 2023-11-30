2e8f09aefb53ff589d2e38c518bbb8ba74532024
aftab.khan <aftab.khan7030@gmail.com>
2023-11-30 12:18:56 +0500

    modified db name

--- src/db/dbconn.js
+++ src/db/dbconn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Dbconnection = async ()=>{
     try {
-    const connect = await mongoose.connect("mongodb://localhost:27017/men100m-api")
+    const connect = await mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`)
     console.log("DB is connected successfully")
     } catch (error) {
       console.log(error)  
